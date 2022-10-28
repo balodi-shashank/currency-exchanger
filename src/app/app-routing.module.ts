db672b2cc6a1840eb5138dcdf52265998906adbb
balodi-shashank <balodishashank@gmail.com>
2022-10-28 21:27:30 +0530

    base level changes added: storage service language service convertor template common css files basic routes for home, details view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConvertorComponent } from './components/convertor/convertor.component';
+import { DetailsViewComponent } from './components/details-view/details-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/convertor',
+    pathMatch: 'full',
+  },
+  {
+    path: 'details',
+    component: DetailsViewComponent
+  },
+  {
+    path: 'convertor',
+    component: ConvertorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { DetailsViewComponent } from './components/details-view/details-view.component';
+import { ConvertorComponent } from './components/convertor/convertor.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsViewComponent,
+    ConvertorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient],
+        },
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
